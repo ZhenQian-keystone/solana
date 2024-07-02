ce37a83dd3454cf87b7a6f70b386779cc6a24a20
ZhenQian <zhenqian@keyst.one>
2024-07-02 18:50:29 +0800

    fix: add version tx raw message

--- get_raw_tx_example2.js
+++ get_raw_tx_example2.js
@@ -1,5 +1,6 @@
 // https://solscan.io/tx/4VkVEYiRqS1y3XoMKXBXTRbffyjHkpj6LAz4N55K6oV4xCRQuowXDJHSPtgC6dtoaSrZ2NkNFkX83FYsovRRgVnH
 
+// this is a versioned transaction
 const web3 = require('@solana/web3.js')
 
 const connection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'))
@@ -20,7 +21,13 @@ connection
 
 		console.log('signatures: ', signatures)
 
+		let raw_message = transaction.message.serialize()
+
+		// 800100020305919998ccad85d7254856bde658b1926b77276f9c9f93d4713eec50c6f0cd4e0306466fe5211732ffecadba72c39be7bc8ce5bbc5f7126b2c439b3a4000000008239266ec3629985acb2ec387b47c3c3ca057181773835b4f0e99ab4f065e7dc3e93416d06d11ad5352987624986db3240b02ea5cc42ba3c2e9f3a6e9bfe1c203010005028c45080001000903208601000000000002271d1f1e00040508070906191c19141b11120c0f1a0b0a0e13101020070d00180315000d04171619295bbffbf792f60aa2084d230100000000ad88c11900000000030201070000000000000101000000000004fdd750c4799429e7e17c0c7cf0a55a82d70846d1bc97665714af810651e8bb04010e060d010c0f0709209be863b24cb2303c7162670000dfaf3176a1ada32a0305df593d8a935c7dcd06038b8d118a8c0287868cb7d386ca1b4cb4125557bf989073c37d63ae73104247ee1280afa86a8ac75b0b9695938f97949a9192988d019bfddf37b259d3ceada480a9a43ba12f21bfd3d7afd64d35b9f0330f6645d3016f03969a9900
+		console.log('raw message: ', Buffer.from(raw_message).toString('hex'))
+
 		let message = transaction.message
+		let raw = message.serialize
 		// console.log('message ===>  ', message)
 		// for each message
 		let message_header = message.header
